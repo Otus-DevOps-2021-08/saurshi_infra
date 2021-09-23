14f856f6a4e444510c77de4d60979596e36de9c6
Sergey  Shitenkov <s@shitenkov.ru>
2021-09-23 23:00:03 +0300

    Created startup script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o errexit
+
+sudo apt-get install git
+git clone https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
+
---
+++ install_ruby.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -o errexit
+
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+
